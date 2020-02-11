6042725c16fb07293e20c2c249a2099613405a50
RomainCrispini <romain.crispini@gmail.com>
2020-02-11 11:01:08 +0100

    Android App Integration Videos

---
+++ app/src/main/java/com/romain/app9videos/MainActivity.java
@@ -0,0 +1,36 @@
+package com.romain.app9videos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoView = (VideoView) findViewById(R.id.videoView);
+
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.nocesfigaro);
+        videoView.setVideoURI(uri);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        videoView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        videoView.suspend();
+    }
+}
